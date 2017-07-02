4f387ef08745ca89636dbcf9a71a0328c99264bc
jason20081404 <afu000999@gmail.com>
2017-07-02 22:12:35 +0800

    commit helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : he.cpp
+// Author      : qidong
+// Version     :
+// Copyright   : copyright by qidong @Oct, 2016
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
